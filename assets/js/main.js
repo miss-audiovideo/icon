0c73108e668658faef850a86c4ee3ae35e460272
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-11-28 13:20:11 +0530

    fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,7 +100,7 @@ const sr = ScrollReveal({
 });
 
 sr.reveal(`.home__data, .home__img,
-            .about__data, .about__img,
+            .about__data, .cards__img,
             .services__content, .menu__content,
             .app__data, .app__img,
             .contact__data, .contact__button,

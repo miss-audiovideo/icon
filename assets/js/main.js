0b106f0732cca2988a38c347b3daec095d84ec92
Dhruv Dwivedi <dhrvdwvd@protonmail.com>
2021-12-03 18:21:37 +0530

    index-page small fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,7 +91,7 @@ sr.reveal(
             .services__content, .menu__content,
             .app__data, .app__img,
             .contact__data, .contact__button,
-            .footer__content`,
+            .footer__content, .cards`,
   {
     interval: 200,
   }
